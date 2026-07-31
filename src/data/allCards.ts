1a9559d999dca71af34514db92a2d470a010b96c
Samuel Perreault <samuel@MacBook-Air-de-Samuel.local>
2026-07-31 04:45:14 +0200

    10 series noms communs grecs avec article singular plural

--- src/data/allCards.ts
+++ src/data/allCards.ts
@@ -8,9 +8,16 @@ import {
   greekAdjectivesS6, greekAdjectivesS7, greekAdjectivesS8, greekAdjectivesS9, greekAdjectivesS10
 } from './greek'
 
+import {
+  greekNounsS1, greekNounsS2, greekNounsS3, greekNounsS4, greekNounsS5,
+  greekNounsS6, greekNounsS7, greekNounsS8, greekNounsS9, greekNounsS10
+} from './greek'
+
 export const allCards = [
   ...greekVerbsS1, ...greekVerbsS2, ...greekVerbsS3, ...greekVerbsS4, ...greekVerbsS5,
   ...greekVerbsS6, ...greekVerbsS7, ...greekVerbsS8, ...greekVerbsS9, ...greekVerbsS10,
   ...greekAdjectivesS1, ...greekAdjectivesS2, ...greekAdjectivesS3, ...greekAdjectivesS4, ...greekAdjectivesS5,
-  ...greekAdjectivesS6, ...greekAdjectivesS7, ...greekAdjectivesS8, ...greekAdjectivesS9, ...greekAdjectivesS10
+  ...greekAdjectivesS6, ...greekAdjectivesS7, ...greekAdjectivesS8, ...greekAdjectivesS9, ...greekAdjectivesS10,
+  ...greekNounsS1, ...greekNounsS2, ...greekNounsS3, ...greekNounsS4, ...greekNounsS5,
+  ...greekNounsS6, ...greekNounsS7, ...greekNounsS8, ...greekNounsS9, ...greekNounsS10
 ]
--- src/data/greek/index.ts
+++ src/data/greek/index.ts
@@ -7,3 +7,8 @@ export {
   greekAdjectivesS1, greekAdjectivesS2, greekAdjectivesS3, greekAdjectivesS4, greekAdjectivesS5,
   greekAdjectivesS6, greekAdjectivesS7, greekAdjectivesS8, greekAdjectivesS9, greekAdjectivesS10
 } from './adjectives'
+
+export {
+  greekNounsS1, greekNounsS2, greekNounsS3, greekNounsS4, greekNounsS5,
+  greekNounsS6, greekNounsS7, greekNounsS8, greekNounsS9, greekNounsS10
+} from './nouns'
